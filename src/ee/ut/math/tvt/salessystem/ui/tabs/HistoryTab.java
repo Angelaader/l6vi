d7fc92c6e8f70f05294639c678190894e8ead7ee
annetts <Annett.Saarik@hotmail.com>
2014-10-26 23:51:56 +0200

    Merge

--- src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
+++ src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
@@ -1,22 +1,37 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import java.awt.Component;
+import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
  * labelled "History" in the menu).
  */
 public class HistoryTab {
-    
-    // TODO - implement!
-
-    public HistoryTab() {} 
+	
+	SalesSystemModel model;
+	PurchaseInfoTableModel purchase;
+	JTable soldItemTable;
+	JTable purchasesTable;
+	
+    public HistoryTab(SalesSystemModel model) {
+    	this.model = model;
+    } 
     
     public Component draw() {
         JPanel panel = new JPanel();
-        // TODO - Sales history tabel
+        panel.setLayout(new GridBagLayout());
+        
+        
+        
+        
+        
         return panel;
     }
 }
