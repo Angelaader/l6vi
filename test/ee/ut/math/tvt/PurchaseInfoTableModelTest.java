60059b020a5b230b39574494c1e9c48c9344f471
Angelaader <angela.ader2@gmail.com>
2014-11-23 14:59:30 +0200

    poolik kolmas test

--- test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
+++ test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
@@ -5,15 +5,34 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
+
 public class PurchaseInfoTableModelTest {
 
+	private PurchaseInfoTableModel pm;
+	private SoldItem item1;
+	private SoldItem item2;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
+		pm = new PurchaseInfoTableModel();
+		item1 = new SoldItem(new StockItem(3L,"e","3",10,7),2);
+		item2 = new SoldItem(new StockItem(2L,"b","7",8,3),1);
 	}
-
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetItem(){
+		pm.addItem(item1);
+		pm.addItem(item2);
+		assertEquals(item2, pm.getItemById(2L));
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void testGetItemException(){
+		pm.getItemById(7L);
 	}
 
 }
