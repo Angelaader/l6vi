08c2d734e560a402f53a0027df53454f60737ad2
Jarmo-Hendrik Hints <Malbidion@gmail.com>
2014-11-23 23:23:03 +0200

    fixed non working tests, completed other tests

--- test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
+++ test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.NoSuchElementException;
 import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
